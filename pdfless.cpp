19642395410502977d0832d5fbd7f6828d69711b
Yasuaki MORITA <zousandazou@gmail.com>
2017-06-16 10:37:24 +0900

    moromoro

--- pdfless.cpp
+++ pdfless.cpp
@@ -10,73 +10,193 @@ private:
   const char *m_path;
   aa_context *m_aa;
   poppler::document *m_doc;
-  int m_pages;
-  bool m_reverse;
+  poppler::page *m_cur;
+  int m_page;
+  double m_offx, m_offy, m_scale;
+  poppler::image m_img;
+  bool m_reverse, m_help;
 
 public:
   pdfless(const char *path) {
     using namespace std;
+    using namespace poppler;
     m_path = path;
-    m_doc = poppler::document::load_from_file(path);
+    m_doc = document::load_from_file(path);
     m_aa = aa_autoinit(&aa_defparams);
     if (!m_aa)
       cerr << "aalib init failed" << endl, exit(1);
-    m_pages = m_doc->pages();
     m_reverse = false;
+    m_page = 0;
+    m_cur = m_doc->create_page(m_page);
+    if (!m_cur)
+      cerr << "pdf have no page" << endl, exit(1);
+    reinit();
   }
   ~pdfless(void) {
     aa_close(m_aa);
     delete m_doc;
   }
-  int render(int pagenum, int cx, int cy, double zoom) {
+  void left(void) {
+    m_offx -= 18;
+    normalize();
+  }
+  void right(void) {
+    m_offx += 18;
+    normalize();
+  }
+  void up(void) {
+    m_offy -= 18;
+    normalize();
+  }
+  void down(void) {
+    m_offy += 18;
+    normalize();
+  }
+  void prev_page(void) {
+    --m_page;
+    if (m_page < 0)
+      m_page = 0;
+    if (m_cur)
+      delete m_cur;
+    m_cur = m_doc->create_page(m_page);
+    reinit();
+    normalize();
+    update_image();
+  }
+  void next_page(void) {
+    m_page++;
+    if (m_page >= m_doc->pages())
+      m_page = m_doc->pages() - 1;
+    if (m_cur)
+      delete m_cur;
+    m_cur = m_doc->create_page(m_page);
+    reinit();
+    normalize();
+    update_image();
+  }
+  void zoom_in(void) {
+    double pre = m_scale;
+    m_scale *= 1.25;
+    if (m_scale > 32)
+      m_scale = 32;
+    if (m_scale < 1)
+      m_scale = 1;
+    m_offx *= m_scale / pre;
+    m_offy *= m_scale / pre;
+    normalize();
+    update_image();
+  }
+  void zoom_out(void) {
+    double pre = m_scale;
+    m_scale /= 1.25;
+    if (m_scale > 32)
+      m_scale = 32;
+    if (m_scale < 1)
+      m_scale = 1;
+    m_offx *= m_scale / pre;
+    m_offy *= m_scale / pre;
+    normalize();
+    update_image();
+  }
+  void update_image(void) {
     using namespace poppler;
-    int aw = aa_imgwidth(m_aa);
-    int ah = aa_imgheight(m_aa);
-    double mw = aa_mmwidth(m_aa);
-    double mh = aa_mmheight(m_aa);
-    page *page = m_doc->create_page(pagenum);
-    double rw = page->page_rect().width();
-    double rh = page->page_rect().height();
-    double base = aw/rw * 30.0;
     page_renderer pr;
-    image img = pr.render_page(page, 2 * base * zoom, base * zoom);
-    int iw = img.width();
-    int ih = img.height();
-    int bw = img.bytes_per_row();
-    int offx = (aw - iw + (int)(cx * 0.5 * base))/2;
-    int offy = (ah - ih + (int)(cy * base))/2;
-    image::format_enum fmt = img.format();
-    unsigned char *dat = (unsigned char *)img.data();
-    for (int ay = 0; ay < ah; ay++) {
-      int iy = ay - offy;
-      for (int ax = 0; ax < aw; ax++) {
-        int ix = ax - offx;
-        unsigned b = 0;
-        if (ix < 0 || iw <= ix || iy < 0 || ih <= iy)
-          b = 0;
-        else
+    aa_resize(m_aa);
+    double base;
+    if (aa_imgwidth(m_aa) * m_cur->page_rect().height() <
+        aa_imgheight(m_aa) * m_cur->page_rect().width())
+      base = aa_imgwidth(m_aa) / m_cur->page_rect().width();
+    else
+      base = aa_imgheight(m_aa) / m_cur->page_rect().height();
+    m_img = pr.render_page(m_cur, m_scale * base * 144, m_scale * base * 72, -1,
+                           -1, -1, -1);
+  }
+  void reinit(void) {
+    using namespace poppler;
+    page_renderer pr;
+    m_scale = 1.0;
+    double base;
+    if (aa_imgwidth(m_aa) * m_cur->page_rect().height() <
+        aa_imgheight(m_aa) * m_cur->page_rect().width()) {
+      base = aa_imgwidth(m_aa) / m_cur->page_rect().width();
+      m_img = pr.render_page(m_cur, m_scale * base * 144, m_scale * base * 72,
+                             -1, -1, -1, -1);
+      m_offx = aa_imgwidth(m_aa) / 2;
+      m_offy = m_img.height() / 2;
+    } else {
+      base = aa_imgheight(m_aa) / m_cur->page_rect().height();
+      m_img = pr.render_page(m_cur, m_scale * base * 144, m_scale * base * 72,
+                             -1, -1, -1, -1);
+      m_offx = m_img.width() / 2;
+      m_offy = aa_imgheight(m_aa) / 2;
+    }
+  }
+  void normalize(void) {
+    int iw = aa_imgwidth(m_aa);
+    int ih = aa_imgheight(m_aa);
+    if (m_offx < 0)
+      m_offx = 0;
+    if (m_offx >= m_img.width())
+      m_offx = m_img.width() - 1;
+    if (m_offy < 0)
+      m_offy = 0;
+    if (m_offy > m_img.height())
+      m_offy = m_img.height() - 1;
+  }
+
+  void reverse(void) { m_reverse = !m_reverse; }
+  int render(void) {
+    using namespace poppler;
+    page_renderer pr;
+    aa_resize(m_aa);
+    int iw = aa_imgwidth(m_aa);
+    int ih = aa_imgheight(m_aa);
+    int bw = m_img.bytes_per_row();
+    image::format_enum fmt = m_img.format();
+    unsigned char *dat = (unsigned char *)m_img.data();
+    for (int iy = 0; iy < ih; iy++)
+      for (int ix = 0; ix < iw; ix++) {
+        int tx = ix + m_offx - iw / 2;
+        int ty = iy + m_offy - ih / 2;
+        if (tx < 0 || tx >= m_img.width() || ty < 0 || ty >= m_img.height()) {
+          aa_putpixel(m_aa, ix, iy, 0);
+        } else {
+          int b;
           switch (fmt) {
           case image::format_mono:
-            b = dat[bw * iy + ix];
+            b = dat[bw * ty + tx];
             break;
           case image::format_rgb24:
-            b = brightness(dat[bw * iy + 3 * ix + 0], dat[bw * iy + 3 * ix + 1],
-                           dat[bw * iy + 3 * ix + 2]);
+            b = brightness(dat[bw * ty + 3 * tx + 0], dat[bw * ty + 3 * tx + 1],
+                           dat[bw * ty + 3 * tx + 2]);
             break;
           case image::format_argb32:
-            b = brightness(dat[bw * iy + 4 * ix + 0], dat[bw * iy + 4 * ix + 1],
-                           dat[bw * iy + 4 * ix + 2]);
+            b = brightness(dat[bw * ty + 4 * tx + 0], dat[bw * ty + 4 * tx + 1],
+                           dat[bw * ty + 4 * tx + 2]);
             break;
           case image::format_invalid:
           default:
             return -1;
           }
-        aa_putpixel(m_aa, ax, ay, b);
+          aa_putpixel(m_aa, ix, iy, b);
+        }
       }
+    aa_fastrender(m_aa, 0, 0, aa_scrwidth(m_aa), aa_scrheight(m_aa));
+    aa_printf(m_aa, 0, aa_scrheight(m_aa) - 1, AA_NORMAL,
+              "zoom = %f%% x = %f%% y = %f%% ", m_scale * 100,
+              m_offx / m_img.width() * 100, m_offy / m_img.height() * 100);
+    if (m_help) {
+      aa_printf(m_aa, 0, 0, AA_NORMAL, "j : down ");
+      aa_printf(m_aa, 0, 1, AA_NORMAL, "k : up ");
+      aa_printf(m_aa, 0, 2, AA_NORMAL, "l : right ");
+      aa_printf(m_aa, 0, 3, AA_NORMAL, "h : left ");
+      aa_printf(m_aa, 0, 4, AA_NORMAL, "d <space> : next page ");
+      aa_printf(m_aa, 0, 5, AA_NORMAL, "u : prev page ");
+      aa_printf(m_aa, 0, 6, AA_NORMAL, "+ : zoom in ");
+      aa_printf(m_aa, 0, 7, AA_NORMAL, "- : zoom out ");
+      aa_printf(m_aa, 0, 8, AA_NORMAL, "r : reverse color ");
+      aa_printf(m_aa, 0, 9, AA_NORMAL, "q : quit ");
     }
-    aa_fastrender(m_aa, 0, 0, aa_scrwidth(m_aa),
-              aa_scrheight(m_aa));
-    delete page;
     return 0;
   }
   int clip(int c) {
@@ -93,86 +213,52 @@ public:
     return l;
   }
   void flush(void) { aa_flush(m_aa); }
-  void help(void) {
-    aa_printf(m_aa, 0, 0, AA_NORMAL, "j : down\n");
-    aa_printf(m_aa, 0, 1, AA_NORMAL, "k : up\n");
-    aa_printf(m_aa, 0, 2, AA_NORMAL, "l : right\n");
-    aa_printf(m_aa, 0, 3, AA_NORMAL, "h : left\n");
-    aa_printf(m_aa, 0, 4, AA_NORMAL, "d <space> : next page\n");
-    aa_printf(m_aa, 0, 5, AA_NORMAL, "u : prev page\n");
-    aa_printf(m_aa, 0, 6, AA_NORMAL, "+ : zoom in\n");
-    aa_printf(m_aa, 0, 7, AA_NORMAL, "- : zoom out\n");
-    aa_printf(m_aa, 0, 8, AA_NORMAL, "r : reverse color\n");
-    aa_printf(m_aa, 0, 9, AA_NORMAL, "q : quit\n");
-    flush();
-    getchar();
-  }
-  int run(double zoom) {
-    using namespace poppler;
-    int pagenum = 0;
-    int cx = 0;
-    int cy = 0;
-    if (zoom > 100)
-        zoom = 100;
-    if (zoom < 1)
-        zoom = 1;
-    while (1) {
-      int ret;
-      aa_resize(m_aa);
-      ret = render(pagenum, cx, cy, zoom);
-      if (ret)
-        return ret;
-      flush();
-      switch (getchar()) {
-      case 'j':
-        cy -= 1;
-        break;
-      case 'd':
-      case ' ':
-        if (pagenum < m_pages - 1)
-          pagenum++;
-        break;
-      case 'u':
-        if (0 < pagenum)
-          --pagenum;
-        break;
-      case 'k':
-        cy += 1;
-        break;
-      case 'h':
-        cx += 4;
-        break;
-      case 'l':
-        cx -= 4;
-        break;
-      case '+':
-        if (zoom < 100)
-          zoom *= 1.1;
-        break;
-      case '-':
-        if (0.1 < zoom)
-          zoom /= 1.1;
-        break;
-      case 'r':
-        m_reverse = !m_reverse;
-        break;
-      case '?':
-        help();
-        break;
-      case 'q':
-        return 0;
-      }
-    }
-  }
+  void help(void) { m_help = !m_help; }
 };
 
 int main(int argc, char **argv) {
   using namespace std;
   if (argc < 2)
     cerr << "Please input pdf file" << endl, exit(1);
-  double zoom = 1.0;
-  pdfless pdfless(argv[1]);
-  if (argc == 3)
-    zoom = stod(argv[2]);
-  return pdfless.run(zoom);
+  pdfless pl(argv[1]);
+  while (1) {
+    if (pl.render())
+        exit(1);
+    pl.flush();
+    switch (getchar()) {
+    case 'j':
+      pl.down();
+      break;
+    case 'd':
+    case ' ':
+      pl.next_page();
+      break;
+    case 'u':
+      pl.prev_page();
+      break;
+    case 'k':
+      pl.up();
+      break;
+    case 'h':
+      pl.left();
+      break;
+    case 'l':
+      pl.right();
+      break;
+    case '+':
+      pl.zoom_in();
+      break;
+    case '-':
+      pl.zoom_out();
+      break;
+    case 'r':
+      pl.reverse();
+      break;
+    case '?':
+      pl.help();
+      break;
+    case 'q':
+      return 0;
+    }
+  }
 }
